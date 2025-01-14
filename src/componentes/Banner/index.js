0e35f4662d0edc3df57553606b99189cd8038b1f
Erick Palombo <erick_palombo_@hotmail.com>
2025-01-14 16:44:15 -0300

    banner alterado

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -3,13 +3,13 @@ import styles from './Banner.module.css'
 function Banner() {
   return (
     <div className={styles.container}>
-      <div className={styles.overlay}/>
+      <div className={styles.overlay} />
       <div className={styles.text}>
-        <h1>SEO com React</h1>
-        <p>Eu to aqui pra nesse vídeo dizer que a gente vai aprender a começar uma app inspirada no desenho Pokémon com Nextjs e React, ver algumas dicas sobre performance e de quebra conhecer uma plataforma sensacional pra fazer deploy que é a Vercel. Tudo em 22 minutos nesse vídeo feito com todo o carinho do mundo construindo uma "Pokedex"!    
+        <h1>Sobre o projeto</h1>
+        <p>Este projeto foi desenvolvido utilizando React e utiliza o json-server para simular o consumo de uma API externa. Na página principal, os usuários podem visualizar todos os vídeos organizados em suas respectivas categorias, proporcionando uma navegação intuitiva. Além disso, na segunda página, é possível adicionar novos vídeos por meio de um formulário simples e funcional.
         </p>
       </div>
-      <img className={styles.image} src='./images/player.png' alt='player' />
+      <img className={styles.image} src='./images/noxus.jpg' alt='player' />
     </div>
   )
 }
